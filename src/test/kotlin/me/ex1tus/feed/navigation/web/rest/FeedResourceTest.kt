0fc1e9309a7805d319822a79815828202c88dfaf
Alexander Romanov <ex1tus@yandex.ru>
2022-12-19 00:04:12 +0400

    Remove redundant annotation

--- src/test/kotlin/me/ex1tus/feed/navigation/web/rest/FeedResourceTest.kt
+++ src/test/kotlin/me/ex1tus/feed/navigation/web/rest/FeedResourceTest.kt
@@ -9,18 +9,15 @@ import me.ex1tus.feed.navigation.service.FeedService
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
-@ExtendWith(SpringExtension::class)
 class FeedResourceTest {
 
     @TestConfiguration
